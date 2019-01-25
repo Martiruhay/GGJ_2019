6a16c090de18596c749339181c977182a47772f5
GeriBP <gbelenguer@gmail.com>
2019-01-25 23:25:09 +0100

    Packages+Asssets

---
+++ Assets/RotateTowards.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowards : MonoBehaviour {
+    public GameObject target;
+    public float smooth;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Vector3 dir = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), smooth);
+    }
+}
