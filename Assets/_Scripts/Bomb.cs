56bf5622697845e6a68d145587a5465431865e6f
Martiruhay <marti-nini@hotmail.com>
2019-01-27 04:13:59 +0100

    Add stains real scene2

--- Assets/_Scripts/Bomb.cs
+++ Assets/_Scripts/Bomb.cs
@@ -121,6 +121,7 @@ public class Bomb : MonoBehaviour
             {
                 p.HitBomb();
                 Explode();
+                InstantiateStain();
             }
         }
         else
