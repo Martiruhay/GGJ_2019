4c5c89d609adf79c3f459498a685c9539c1b778e
GeriBP <gbelenguer@gmail.com>
2019-01-27 11:56:03 +0100

    cloudz

---
+++ Assets/_Scripts/Cloud.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float speedMin, speedMax;
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(speedMin, speedMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+        if (transform.position.x < -52f)
+        {
+            transform.position = new Vector3(-transform.position.x, transform.position.y, 0f);
+        }
+    }
+}
