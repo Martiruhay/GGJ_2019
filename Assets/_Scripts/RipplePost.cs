8fedcb9d398f9f2e5ab77f121192a35f94b8ed8b
GeriBP <gbelenguer@gmail.com>
2019-02-04 09:31:39 +0100

    ripple (deactivated)

---
+++ Assets/_Scripts/RipplePost.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RipplePost : MonoBehaviour
+{
+    public Material RippleMaterial;
+    public float MaxAmount = 50f;
+
+    [Range(0, 1)]
+    public float Friction = .9f;
+
+    private float Amount = 0f;
+    private Camera cam;
+
+    public static RipplePost instance;
+
+    private void Start()
+    {
+        instance = this;
+        cam = GetComponent<Camera>();
+    }
+
+    public void SetPoint(Vector3 pos)
+    {
+        pos = cam.WorldToScreenPoint(pos);
+        this.Amount = this.MaxAmount;
+        this.RippleMaterial.SetFloat("_CenterX", pos.x);
+        this.RippleMaterial.SetFloat("_CenterY", pos.y);
+    }
+
+    void Update()
+    {
+        this.RippleMaterial.SetFloat("_Amount", this.Amount);
+        this.Amount *= this.Friction;
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, this.RippleMaterial);
+    }
+}
